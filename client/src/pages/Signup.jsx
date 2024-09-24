c7e17af2b89cdaeb31a707e8272da794e8e3d3e0
Zarana Bavishi <zarna.jenish22c@gmail.com>
2024-09-24 00:04:03 -0400

    Fix: Login and sign up page UI same type so here i created common new loginsingup.css file

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Layout from '../components/Layout.jsx';
 import { Link } from "react-router-dom";
+import '../css/loginsignup.css'; 
 
 function Signup() {
   return (
