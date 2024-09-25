6b80ab3b6ce41599498c08bc2310831d246416e2
Parthi <Pbharadva7050@conestogac.on.ca>
2024-09-24 20:54:13 -0400

    added css for the Home page and bug fixing for the routes to navigate to home page.

--- client/src/route.js
+++ client/src/route.js
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
+import Home from "./pages/HomePage";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 
 
 const router = createBrowserRouter([
+    {
+    path: "/HomePage",
+    element: <Home />,
+    },
     {
         path:'/',
         element: <Login />,
