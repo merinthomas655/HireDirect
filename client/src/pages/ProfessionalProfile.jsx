544d4d4455c2753b17978947a412236d3831569a
merinthomas655 <merinthomas655@gmail.com>
2024-09-27 16:46:28 -0400

    professional-profile : Basic page structure created

---
+++ client/src/pages/ProfessionalProfile.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+function ProfessionalProfile() {
+  return (
+    <Layout>
+      <div className="professional-profile">
+        <h1>Professional Profile</h1>
+      </div>
+    </Layout>
+  );
+}
+
+export default ProfessionalProfile;
