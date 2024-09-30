929fa2dcad5e471964892ff73c1e7fca89582fb4
merinthomas655 <merinthomas655@gmail.com>
2024-09-29 22:25:24 -0400

    professional-profile : Created Reviews section component

---
+++ client/src/components/ReviewsSection.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const ReviewsSection = ({ reviews }) => {
+  const reviewCards = [];
+  for (let i = 0; i < reviews.length; i++) {
+    reviewCards.push(
+      <div className="review-card" key={reviews[i].id}>
+        <p>Rating: {reviews[i].rating} stars</p>
+        <p>{reviews[i].comment}</p>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      {reviewCards.length > 0 ? reviewCards : <p>No reviews available.</p>}
+    </div>
+  );
+};
+
+export default ReviewsSection;
