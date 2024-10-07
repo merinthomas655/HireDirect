bbeae89dfe2e82855c8a54f36ae351e3a0f33660
Zarana Bavishi <zarna.jenish22c@gmail.com>
2024-10-07 14:59:11 -0400

    Booking page UI Integration

--- client/src/pages/Booking.jsx
+++ client/src/pages/Booking.jsx
@@ -89,11 +89,64 @@ function Booking() {
                 <input type="number" placeholder="Price" />
               </div>
             </div>
-           
-            
+            <div className="map-container">
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10221171.22506629!2d-115.01853681319143!3d58.268952913515065!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4b0d03d337cc6ad9%3A0x9968b72aa2438fa5!2sCanada!5e0!3m2!1sen!2sin!4v1728115201743!5m2!1sen!2sin"
+                width="100%"
+                height="350"
+                style={{ border: 0, borderRadius: "4px" }}
+                allowFullScreen=""
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+              ></iframe>
+              <div className="address-box">
+                <div className="booking-form-field address-input">
+                  <input type="text" placeholder="Address" />
+                </div>
+              </div>
+            </div>
+            <div className="selected-service-box-desktop">
+              <h3 className="selected-service-title">Selected Service:</h3>
+              <div className="selected-slote-price-wrapper">
+                <div className="selected-slote-price">
+                  <div className="selected-slote-price-title">
+                    Service 1 (Description , Price)
+                  </div>
+                  <div className="selected-slote-price-filed">
+                    <div className="booking-form-field">
+                      <input type="time" placeholder="Available Slots" />
+                    </div>
+                  </div>
+                </div>
+                <div className="selected-slote-price">
+                  <div className="selected-slote-price-title">
+                    Service 1 (Description , Price)
+                  </div>
+                  <div className="selected-slote-price-filed">
+                    <div className="booking-form-field">
+                      <input type="time" placeholder="Available Slots" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-     
+        <div className="payment-box">
+          <h3 className="payment-title">Payment</h3>
+          <div className="booking-form-field select-filed">
+            <select name="paymet-type" id="" defaultValue="Payment opt">
+              <option value="Payment opt" disabled hidden>
+                Payment Options
+              </option>
+              <option value="online">Online</option>
+              <option value="test">test</option>
+            </select>
+          </div>
+        </div>
+        <div className="confirm-booking-btn-wrapper">
+          <button className="confirm-booking-btn">Confirm Booking</button>
+        </div>
       </div>
     </Layout>
   );
