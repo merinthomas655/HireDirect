b2dd97aa5ccb54274964066ac1000d21a88a637c
ParthiBharadva <parthibharadva@gmail.com>
2024-10-14 19:14:38 -0400

    Update css for dashboard provider page and  some big fixing in home page css

--- client/src/route.js
+++ client/src/route.js
@@ -4,6 +4,7 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import SessionRoute from "./components/SessionRoute";
 import Booking from "./pages/Booking";
+import ProviderDashboard from "./pages/ProviderDashboard";
 
 
 const router = createBrowserRouter([
@@ -13,7 +14,7 @@ const router = createBrowserRouter([
     },
     {
         path:'/',
-        element: <Login />,
+        element: <Home />,
     },
     {
         path:'/login',
