b314aa8309f85bf2b81be926f292892fcd9ff91a
Zarana Bavishi <zarna.jenish22c@gmail.com>
2024-10-14 13:50:46 -0400

    fix: Aman removed my code to resolve conflict. I added code again

--- server/resolvers/resolvers.js
+++ server/resolvers/resolvers.js
@@ -2,6 +2,7 @@ const User = require('../models/User');
 const Provider = require('../models/Provider');
 const Category = require('../models/Category');
 const Review = require('../models/Review');
+const bcrypt = require('bcrypt');
 
 const resolvers = {
   Query: {
@@ -74,6 +75,107 @@ const resolvers = {
       }
     },
   },
+
+  Mutation: {
+
+    //This is login Mutation
+
+    login: async (_, { email, password }) => {
+      try {
+        // Here check if user sends data null or not
+        if (!email || email.trim() === "" ||
+          !password || password.trim() === "") {
+          return {
+            user: null,
+            message: 'Please add all required fields data',
+            success: false,
+          };
+        }
+
+        // Check here email ID exit or not
+        const user = await User.findOne({ email });
+
+        if (!user) {
+          return {
+            user: null,
+            message: "This emill ID does not found",
+            success: false,
+          };
+        }
+
+        //Check user enter password with exit password
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+          return {
+            user: null,
+            message: 'Invalid Password',
+            success: false,
+          };
+        }
+
+        return {
+          user: user,
+          message: 'Login successfully',
+          success: true,
+        };
+      } catch (error) {
+        throw new Error('Internal server error' + error);
+      }
+    },
+
+    //This is singup mutation
+
+    signup: async (_, { username, email, password, role }) => {
+      try {
+        // Here check if user sends data null or not
+        if (!username || username.trim() === "" ||
+          !email || email.trim() === "" ||
+          !password || password.trim() === "" ||
+          !role || role.trim() === "") {
+          return {
+            user: null,
+            message: 'Please add all required fields data',
+            success: false,
+          };
+        }
+
+        // Check if the email already exists
+        const user = await User.findOne({ email });
+        if (user) {
+          return {
+            user: null,
+            message: 'This email ID already exists',
+            success: false,
+          };
+        }
+
+        // Here check user role match or not 
+        if (!['user', 'provider'].includes(role.toLowerCase())) {
+          return {
+            user: null,
+            message: 'Invalid role provided',
+            success: false,
+          };
+        }
+
+        const password_hash = await bcrypt.hash(password, 10);
+
+        // Create user and save in database
+        const signupUser = new User({ username, email, password: password_hash, role });
+        await signupUser.save();
+
+        return {
+          user: signupUser,
+          message: 'Signup successfully',
+          success: true,
+        };
+      } catch (error) {
+        throw new Error('Internal server error' + error);
+      }
+    },
+  },
+
 };
 
 module.exports = resolvers;
