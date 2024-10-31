e2c6ca3a331da8deec7ae61c17db331b49214f7b
Merin Pandiyalackal Thomas <merinthomas655@gmail.com>
2024-10-31 11:30:53 -0400

    Update route.js added User Dashboard Route

--- client/src/route.js
+++ client/src/route.js
@@ -5,6 +5,7 @@ import Signup from "./pages/Signup";
 import SessionRoute from "./components/SessionRoute";
 import Booking from "./pages/Booking";
 import ProviderDashboard from "./pages/ProviderDashboard";
+import UserDashboard from "./pages/UserDashboard";
 import ServicePage from "./pages/ServicePage";
 import BookingConfirmation from "./pages/BookingConfirmation";
 import ProfessionalProfile from "./pages/ProfessionalProfile";
@@ -37,6 +38,10 @@ const router = createBrowserRouter([
     {
         path:'/providerdashboard',
         element: <ProviderDashboard />,
+    },
+    {
+        path:'/userdashboard',
+        element: <UserDashboard />,
     },
     {
         path:'/bookingconfirmation',
