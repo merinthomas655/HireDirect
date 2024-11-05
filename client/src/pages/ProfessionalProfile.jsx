090a1226e987b94315ef86f6c9b911796f8ff1ab
merinthomas655 <merinthomas655@gmail.com>
2024-11-05 10:37:50 -0500

    Changed image path to absolute path

--- client/src/pages/ProfessionalProfile.jsx
+++ client/src/pages/ProfessionalProfile.jsx
@@ -1,22 +1,28 @@
 import React from "react";
+import Layout from '../components/Layout.jsx';
 import { useQuery } from '@apollo/client';
 import { GET_PROVIDER_PROFILE } from '../graphql/queries';
-import { useParams } from 'react-router-dom';
+import { useNavigate,useParams } from 'react-router-dom';
 import ReviewsSection from '../components/ReviewsSection';
 import ServicesTable from '../components/ServicesTable';
-import Layout from '../components/Layout';
+
 import "../css/professionalprofile.css";
 import "../css/servicestable.css";
 import "../css/reviewssection.css";
 
 function ProfessionalProfile() {
   const { providerId } = useParams();
+  const navigate = useNavigate();
   const { loading, error, data } = useQuery(GET_PROVIDER_PROFILE, { variables: { id: providerId } });
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
 
   const { provider } = data;
+
+  const handleBookNow = () => {
+    navigate(`/booking?providerId=${providerId}`);
+  };
   return (
     <Layout>
       <div className="professional-profile">
@@ -31,7 +37,7 @@ function ProfessionalProfile() {
             <p className="bio">{provider.bio}</p>
             <p className="rating">Rating: {provider.ratings}</p>
             <p className="location">Location: {provider.location.address}</p>
-            <button className="book-now-button">Book Now</button>
+            <button className="book-now-button" onClick={handleBookNow}>Book Now</button>
           </div>
         </div>
         <div className="services-section">
