d226e2b2256e5299186c046681122ecaf14b2e54
merinthomas655 <merinthomas655@gmail.com>
2024-11-11 22:34:09 -0500

    user-dashboard-backend : Added graphql query to modify user profile

--- client/src/graphql/queries.js
+++ client/src/graphql/queries.js
@@ -57,3 +57,33 @@ export const FETCH_USER_PROFILE = gql`
   }
 }
 `;
+export const MODIFY_USER_PROFILE = gql`
+  mutation ModifyUserProfile(
+    $id: ID!
+    $username: String
+    $email: String
+    $phone_number: String
+    $address: AddressInput
+    $password: String
+  ) {
+    modifyUserProfile(
+      id: $id
+      username: $username
+      email: $email
+      phone_number: $phone_number
+      address: $address
+      password: $password
+    ) {
+      _id
+      username
+      email
+      phone_number
+      address {
+        street
+        city
+        state
+        zip_code
+      }
+    }
+  }
+`;
