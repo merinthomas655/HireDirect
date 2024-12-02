985847087a20920d825f6ca1ea6ddcd0b2e5d3e4
Zarana Bavishi <zarna.jenish22c@gmail.com>
2024-12-02 15:37:07 -0500

    Login page in send OTP dialog created with responsive view

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,38 +1,94 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Layout from '../components/Layout.jsx';
 import { Link } from "react-router-dom";
+import '../css/loginsignup.css';
+import '../css/forgotpasswordDialog.css';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
-import SpeechRecognition, { useSpeechRecognition } from "react-speech-recognition";
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
+
 
 function Login() {
-  const navigate = useNavigate();
+  const navigate = useNavigate(); // Initialize navigate
 
   const [email, setEmail] = useState('');
-  const [password, setPassword] = useState(''); 
-  const [showPassword, setShowPassword] = useState(false);
-  const { transcript,listening, resetTranscript } = useSpeechRecognition();
-  const [isListening, setIsListening] = useState(false);
+  const [password, setPassword] = useState('');
+  const [message, setMessage] = useState('');
+  const [otptext, setOtp] = useState('');
+  const [newPassword, setnewPassword] = useState('');
+  const [confirmaPassword, setconfirmaPassword] = useState('');
 
-  function startTextToSpeech() {
-    if(isListening){
-      setIsListening(false);
-      SpeechRecognition.stopListening();
-      setEmail(transcript);
-    }else{
-      setIsListening(true);
-      setEmail("");
-      resetTranscript();
-      SpeechRecognition.startListening({ continuous: true });
-     
+  const [showPassword, setShowPassword] = useState(false);
+
+  const [isOtpDialogOpen, setIsOtpDialogOpen] = useState(false);
+  const [isVerifyOtpDialogOpen, setIsVerifyOtpDialogOpen] = useState(false);
+  const [isChangesPasswordDialogOpen, setIsChangesPasswordDialogOpen] = useState(false);
+
+  const [formData, setFormData] = useState({
+    to_name: '',
+    to_email: '',
+    otp: '',
+  });
+
+  const generateOtp = () => Math.floor(100000 + Math.random() * 900000).toString();
+  let otp;
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const sendOtpEmail = async (e) => {
+    e.preventDefault();
+
+    //Check here email validation formate
+    const validateEmail = (email) => {
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      return emailRegex.test(email);
+    };
+
+    if (!formData.to_name || !formData.to_email) {
+      return toast.error('Please provide both name and email.');
+    }
+
+    if (!validateEmail(formData.to_email)) {
+      return toast.error('Please enter a valid email address.');
+    }
+
+    sendOTP();
+  };
+
+  const sendOTP = () => {
+    otp = generateOtp();
+    const updatedFormData = { ...formData, otp };
+
+    try {
+      toast.success("OTP sent successfully in your email ID ");
+      setIsOtpDialogOpen(false);
+      setIsVerifyOtpDialogOpen(true)
+    } catch (error) {
+      toast.error('Failed to send OTP. Please try again.' + error.text);
     }
   }
 
-  useEffect(() => {
-    if (listening) {
-      setEmail(transcript.trim().replace(/\.com\.$/, ".com")); // Automatically update searchInput as the user speaks
+  const verifyOtp = async (e) => {
+    if (otp !== otptext) {
+      toast.error("Invalid OTP. Please try again");
+    } else {
+      toast.success("OTP verify successfully");
+      setIsVerifyOtpDialogOpen(false)
+      setIsChangesPasswordDialogOpen(true)
     }
-  }, [transcript, listening]);
+  }
+
+  const changesPassword = async (e) => {
+    setIsChangesPasswordDialogOpen(false)
+    toast.success("Password changed successfully");
+  }
 
   const showPasswordVisibility = () => {
     setShowPassword(!showPassword);
@@ -47,7 +103,7 @@ function Login() {
     };
 
     if (!validateEmail(email)) {
-      toast.error('Please enter a valid email address.');
+      setMessage('Please enter a valid email address.');
       return;
     }
 
@@ -90,21 +146,19 @@ function Login() {
 
         sessionStorage.setItem('usersession', JSON.stringify(userData));
 
-        toast.success(result.data.login.message || "Login successful!");
-
-        setTimeout(() => {
-          navigate('/HomePage');
-        }, 2000);
+        setMessage(result.data.login.message || "Login successful!");
+        navigate('/HomePage');
       } else {
-        toast.error(result.data.login.message || "Login failed!");
+        setMessage(result.data.login.message || "Login failed!");
       }
     } catch (error) {
-      toast.error("Login failed. Please try again.");
+      setMessage("Login failed. Please try again.");
     }
   };
 
   return (
     <Layout>
+      <ToastContainer position="top-right" autoClose={2000} />
       <div className="login-signup-container">
         <h2 className="login-text">Login</h2>
         <form onSubmit={handleSubmit}>
@@ -117,14 +171,11 @@ function Login() {
               onChange={(e) => setEmail(e.target.value)}
             />
             <span className="icon">
-              {/* <img
+              <img
                 src="./assets/img/email.png"
                 alt="email"
                 className="img-fluid"
-              /> */}
-              <button onClick={() => startTextToSpeech()}>
-                {isListening ? "🛑" : "🎤"}
-                </button>
+              />
             </span>
           </div>
           <div className="input-group-box password-box">
@@ -147,6 +198,7 @@ function Login() {
           <div className="forgot-password">
             <button
               type="button"
+              onClick={() => setIsOtpDialogOpen(true)}
               className="forgot-password-link"
             >
               Forgot Password?
@@ -157,9 +209,53 @@ function Login() {
           </button>
         </form>
 
+        {message && <p className="message-text" style={{ color: 'red' }}>{message}</p>}
+
         <Link to="/signup" className="signup-text">
           Don't have an account? Sign Up
         </Link>
+
+        {isOtpDialogOpen && (
+          <div className="overlay">
+            <div className="dialog">
+              <button
+                onClick={() => setIsOtpDialogOpen(false)}
+                className="close-button"
+              >
+                &times;
+              </button>
+              <h2 className="title">Send OTP</h2>
+              <form onSubmit={sendOtpEmail}>
+                <label className="label">Name</label>
+                <input
+                  type="text"
+                  name="to_name"
+                  value={formData.to_name}
+                  onChange={handleChange}
+                  placeholder="User's Name"
+                  required
+                  className="input"
+                />
+
+                <label className="label">Email</label>
+                <input
+                  type="email"
+                  name="to_email"
+                  value={formData.to_email}
+                  onChange={handleChange}
+                  placeholder="User's Email"
+                  required
+                  className="input"
+                />
+
+                <button type="submit" className="submit-button">
+                  Send OTP
+                </button>
+              </form>
+            </div>
+          </div>
+        )}
+
       </div>
     </Layout>
   );
