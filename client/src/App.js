7d7567cfde405cf7e6401e25d6c18572731933bb
Zarana Bavishi <zarna.jenish22c@gmail.com>
2024-12-11 16:52:26 -0500

    Fix: remove firebase integration

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,6 @@ import {
 } from "react-router-dom";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import analytics from "./firebase/firebase";
 
 function App() {
   return (
